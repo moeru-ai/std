a2145307ecfb9dd8ce8e8d4cd0dd527d32292131
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-04-25 19:58:31 +0800

    chore(std): update

---
+++ eslint.config.ts
@@ -0,0 +1,22 @@
+import { GLOB_MARKDOWN_CODE, GLOB_TESTS } from '@antfu/eslint-config'
+import { defineConfig } from '@importantimport/eslint-config'
+
+export default defineConfig({
+  typescript: { tsconfigPath: './tsconfig.json' },
+}, [{
+  rules: {
+    '@masknet/no-default-error': 'off',
+    '@masknet/no-then': 'off',
+    'sonarjs/todo-tag': 'warn',
+  },
+}, {
+  files: [...GLOB_TESTS, GLOB_MARKDOWN_CODE],
+  rules: {
+    '@masknet/no-top-level': 'off',
+    '@masknet/unicode-specific-set': 'off',
+  },
+}, {
+  ignores: [
+    'cspell.config.yaml',
+  ],
+}])
---
+++ packages/std/src/trampoline/index.ts
@@ -0,0 +1,25 @@
+export type TrampolineFn<T> = (() => Promise<TrampolineFn<T>> | TrampolineFn<T>) | Promise<T> | T
+
+export const trampoline = async <T>(fn: () => TrampolineFn<T>) => {
+  let result = await fn()
+
+  // make ts happy
+  // eslint-disable-next-line @masknet/type-no-instanceof-wrapper, unicorn/no-instanceof-builtins
+  while (result instanceof Function)
+    result = await result()
+
+  return result
+}
+
+export type TrampolineSyncFn<T> = (() => TrampolineSyncFn<T>) | T
+
+export const trampolineSync = <T>(fn: () => TrampolineSyncFn<T>): T => {
+  let result = fn()
+
+  // make ts happy
+  // eslint-disable-next-line @masknet/type-no-instanceof-wrapper, unicorn/no-instanceof-builtins
+  while (result instanceof Function)
+    result = result()
+
+  return result
+}
