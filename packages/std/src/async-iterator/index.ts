950f0b0f914f602f26f339a0639e785d9608f2fa
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-06-24 23:36:34 +0800

    fix(std/async-iterator): fix function name

--- packages/std/src/async-iterator/index.ts
+++ packages/std/src/async-iterator/index.ts
@@ -2,7 +2,7 @@
 // Read more from this: https://caniuse.com/mdn-api_readablestream_--asynciterator
 //
 // eslint-disable-next-line func-style -- this is a generator function
-export async function* eadableStreamToAsyncIterator<T, F = Uint8Array>(res: ReadableStream<F>, func: (value: F) => Promise<T>): AsyncGenerator<T, void, unknown> {
+export async function* readableStreamToAsyncIterator<T, F = Uint8Array>(res: ReadableStream<F>, func: (value: F) => Promise<T>): AsyncGenerator<T, void, unknown> {
   // react js - TS2504: Type 'ReadableStream<Uint8Array>' must have a '[Symbol.asyncIterator]()' method that returns an async iterator - Stack Overflow
   // https://stackoverflow.com/questions/76700924/ts2504-type-readablestreamuint8array-must-have-a-symbol-asynciterator
   const reader = res.getReader()
