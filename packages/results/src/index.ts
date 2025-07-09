151ed58c9e8dc8034d6b0408082000d410d2f470
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-09 20:00:28 +0800

    feat: new package `@moeru/results`

---
+++ packages/results/src/index.ts
@@ -0,0 +1,16 @@
+import type { None, Option, Some } from './option'
+import type { Err, Ok, Result } from './result'
+
+import * as option from './option'
+import * as result from './result'
+
+export {
+  Err,
+  None,
+  Ok,
+  Option,
+  option,
+  result,
+  Result,
+  Some,
+}
