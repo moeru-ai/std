97f23009622d5d00f73759b09d3bb004a378bfe8
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-09 20:02:12 +0800

    chore(std): revert changes

--- packages/std/scripts/update-exports.ts
+++ packages/std/scripts/update-exports.ts
@@ -11,12 +11,18 @@ for (let entry of entries) {
 
 const packageJson = JSON.parse(await readFile('./package.json', 'utf8')) as Record<string, unknown>
 
-packageJson.exports = Object.fromEntries(
-  exports.map(e => [`./${e}`, `./src/${e}/index.ts`]),
-)
+packageJson.exports = Object.fromEntries([
+  ['.', './src/index.ts'],
+  ...exports.map(e => [`./${e}`, `./src/${e}/index.ts`]),
+])
 
 packageJson.publishConfig = {
   exports: Object.fromEntries([
+    ['.', {
+      types: './dist/index.d.ts',
+      // eslint-disable-next-line perfectionist/sort-objects
+      default: './dist/index.js',
+    }],
     ...exports.map(e => [`./${e}`, {
       types: `./dist/${e}/index.d.ts`,
       // eslint-disable-next-line perfectionist/sort-objects
@@ -24,7 +30,14 @@ packageJson.publishConfig = {
     }]),
     ['./package.json', './package.json'],
   ]),
+  main: './dist/index.js',
+  types: './dist/index.d.ts',
 } satisfies Record<string, unknown>
 
 await writeFile('./package.json', `${JSON.stringify(packageJson, null, 2)}\n`)
 console.log('Updated: ./package.json')
+
+const indexTs = exports.map(e => `export * from './${e}'`).join('\n')
+
+await writeFile('./src/index.ts', `${indexTs}\n`)
+console.log('Updated: ./src/index.ts')
