9c87171b133e847b5b3521628b66147c45714798
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-09 19:37:39 +0800

    chore: improve result.unwrap

--- packages/std/src/result/unwrap.ts
+++ packages/std/src/result/unwrap.ts
@@ -1,19 +1,21 @@
 import type { Result } from './core'
 
-import { isOk, isErr } from './is'
+import { isErr, isOk } from './is'
 
 export const unwrap = <T, E>(r: Result<T, E>): T => {
   if (isOk(r))
     return r.value
-  else
+  else if (r.error instanceof Error)
     throw r.error
+  else
+    throw new Error('called `result.unwrap()` on an `Err` value', { cause: r.error })
 }
 
 export const unwrapErr = <T, E>(r: Result<T, E>): E => {
   if (isErr(r))
     return r.error
   else
-    throw new Error('called `result.unwrapErr()` on a `Ok` value', { cause: r.value })
+    throw new Error('called `result.unwrapErr()` on an `Ok` value', { cause: r.value })
 }
 
 export const unwrapOr = <T, E>(r: Result<T, E>, fallback: T): T =>
@@ -26,7 +28,8 @@ export const unwrapOrElse = <T, E>(r: Result<T, E>, onErrValue: (e: E) => T): T 
     ? r.value
     : onErrValue(r.error)
 
+// eslint-disable-next-line sonarjs/no-identical-functions
 export const unwrapOrElseAsync = async <T, E>(r: Result<T, E>, onErrValue: (e: E) => Promise<T>): Promise<T> =>
   isOk(r)
     ? r.value
-    : await onErrValue(r.error)
+    : onErrValue(r.error)
