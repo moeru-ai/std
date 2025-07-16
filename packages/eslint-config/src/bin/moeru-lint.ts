422f2fe4f489c1a358e60fe2ae0778eba3af1802
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-16 21:58:19 +0800

    feat(eslint-config): moeru-lint support path

--- packages/eslint-config/src/bin/moeru-lint.ts
+++ packages/eslint-config/src/bin/moeru-lint.ts
@@ -8,7 +8,8 @@ import { parseArgs } from 'node:util'
 
 import { version } from '../../package.json'
 
-const { values } = parseArgs({
+const { positionals, values } = parseArgs({
+  allowPositionals: true,
   options: {
     'debug': {
       default: false,
@@ -56,18 +57,20 @@ else if (values.help) {
   console.info('Usage: moeru-lint [--fix] [--flag <flag>]')
 }
 else {
+  const path = positionals.at(0) ?? '.'
+
   const fix = values.fix ? '--fix' : ''
   const fixDangerously = values['fix-dangerously'] ? '--fix-dangerously' : ''
   const fixSuggestions = values['fix-suggestions'] ? '--fix-suggestions' : ''
   const cache = values['no-cache'] ? '' : '--cache'
   const flag = values.flag?.map(flag => `--flag ${flag}`).join(' ') ?? ''
 
-  const oxcArgs = [fix, fixDangerously, fixSuggestions]
+  const oxcArgs = [fix, fixDangerously, fixSuggestions, path]
   console.info(`moeru-lint: executing oxlint...\n${values.debug ? oxcArgs.join(' ') : ''}`)
   const oxlint = spawn('oxlint', oxcArgs, { stdio: 'inherit' })
 
   oxlint.on('close', () => {
-    const eslintArgs = [cache, fix, flag]
+    const eslintArgs = [cache, fix, flag, path]
     console.info(`\nmoeru-lint: executing eslint...\n${values.debug ? eslintArgs.join(' ') : ''}`)
     spawn('eslint', [eslintArgs.join(' ')], { stdio: 'inherit' })
   })
