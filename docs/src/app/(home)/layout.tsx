01d8f5a066c246e6fbc11796567a869b76e457bb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-07-23 18:44:33 +0800

    docs: hero

--- docs/src/app/(home)/layout.tsx
+++ docs/src/app/(home)/layout.tsx
@@ -4,7 +4,15 @@ import { HomeLayout } from 'fumadocs-ui/layouts/home'
 
 import { baseOptions } from '@/app/layout.config'
 
-const Layout = ({ children }: PropsWithChildren) =>
-  <HomeLayout {...baseOptions}>{children}</HomeLayout>
+const Layout = ({ children }: PropsWithChildren) => (
+  <HomeLayout
+    {...baseOptions}
+    links={[
+      { text: 'Packages', url: '/docs/packages/std' },
+    ]}
+  >
+    {children}
+  </HomeLayout>
+)
 
 export default Layout
