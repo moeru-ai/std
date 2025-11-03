6a7743e8ccaa6547cd82961b3b3ce18ecc9d7a53
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-03 16:02:47 +0800

    refactor(std/async-iterator)!: skip when supported browser

--- packages/std/src/async-iterator/index.ts
+++ packages/std/src/async-iterator/index.ts
@@ -2,6 +2,10 @@
 // Caniuse: https://caniuse.com/mdn-api_readablestream_--asynciterator
 // WebKit Bugzilla: https://bugs.webkit.org/show_bug.cgi?id=194379
 export async function* readableStreamToAsyncIterator<T>(res: ReadableStream<T>): AsyncGenerator<T, void, unknown> {
+  // eslint-disable-next-line ts/strict-boolean-expressions
+  if (res[Symbol.asyncIterator])
+    yield* res[Symbol.asyncIterator]()
+
   // react js - TS2504: Type 'ReadableStream<Uint8Array>' must have a '[Symbol.asyncIterator]()' method that returns an async iterator - Stack Overflow
   // https://stackoverflow.com/questions/76700924/ts2504-type-readablestreamuint8array-must-have-a-symbol-asynciterator
   const reader = res.getReader()
