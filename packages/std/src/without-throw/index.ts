72088b4f0f6061e78a79011d0db0fb5bc93833ce
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-15 16:45:00 +0800

    refactor: simplify structure, unknown by default

--- packages/std/src/without-throw/index.ts
+++ packages/std/src/without-throw/index.ts
@@ -1,26 +1,24 @@
+export type WithoutThrowResult<T> = { data: T, error: undefined } | { data: undefined, error: unknown }
+
 /** @see {@link https://std.moeru.ai/docs/packages/std/utils/without-throw} */
 export const withoutThrow = <
-  F extends (...args: any[]) => any,
-  E = unknown,
->(func: F) =>
-  (...args: Parameters<F>): { data: ReturnType<F>, error: undefined } | { data: undefined, error: E } => {
-    try {
-      return { data: func(...args), error: undefined }
-    }
-    catch (error) {
-      return { data: undefined, error: error as E }
-    }
+  T extends () => unknown,
+>(fn: T): WithoutThrowResult<ReturnType<T>> => {
+  try {
+    return { data: fn() as ReturnType<T>, error: undefined }
   }
+  catch (error) {
+    return { data: undefined, error }
+  }
+}
 
-export const withoutThrowAsync = <
-  F extends (...args: any[]) => Promise<any>,
-  E = unknown,
->(func: F) =>
-  async (...args: Parameters<F>): Promise<{ data: Awaited<ReturnType<F>>, error: undefined } | { data: undefined, error: E }> => {
-    try {
-      return { data: await func(...args), error: undefined }
-    }
-    catch (error) {
-      return { data: undefined, error: error as E }
-    }
+export const withoutThrowAsync = async <
+  T extends () => Promise<unknown>,
+>(fn: T): Promise<WithoutThrowResult<Awaited<ReturnType<T>>>> => {
+  try {
+    return { data: await fn() as Awaited<ReturnType<T>>, error: undefined }
   }
+  catch (error) {
+    return { data: undefined, error }
+  }
+}
