6d809b4a1204e6d284f1f00656dae2fc557f8640
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-11-15 16:24:31 +0800

    refactor: improve

--- packages/std/src/without-throw/index.ts
+++ packages/std/src/without-throw/index.ts
@@ -2,25 +2,25 @@
 export const withoutThrow = <
   F extends (...args: any[]) => any,
   E = unknown,
->(func: F) => {
-  return (...args: Parameters<F>): { data: ReturnType<F>, error?: undefined } | { data?: undefined, error: E } => {
+>(func: F) =>
+  (...args: Parameters<F>): { data: ReturnType<F>, error: undefined } | { data: undefined, error: E } => {
     try {
-      return { data: func(...args) }
-    } catch (error) {
-      return { error: error as E }
+      return { data: func(...args), error: undefined }
+    }
+    catch (error) {
+      return { data: undefined, error: error as E }
     }
   }
-}
 
 export const withoutThrowAsync = <
   F extends (...args: any[]) => Promise<any>,
   E = unknown,
->(func: F) => {
-  return async (...args: Parameters<F>): Promise<{ data: Awaited<ReturnType<F>>, error?: undefined } | { data?: undefined, error: E }> => {
+>(func: F) =>
+  async (...args: Parameters<F>): Promise<{ data: Awaited<ReturnType<F>>, error: undefined } | { data: undefined, error: E }> => {
     try {
-      return { data: await func(...args) }
-    } catch (error) {
-      return { error: error as E }
+      return { data: await func(...args), error: undefined }
+    }
+    catch (error) {
+      return { data: undefined, error: error as E }
     }
   }
-}
