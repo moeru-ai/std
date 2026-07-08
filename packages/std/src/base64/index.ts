52e7f128ecdad2025b7009412d515d2188527ba2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-07-08 21:26:16 +0800

    refactor(std/base64): baseline 2025

--- packages/std/src/base64/index.ts
+++ packages/std/src/base64/index.ts
@@ -1,2 +1,33 @@
-export { decodeBase64, encodeBase64 } from './base64'
-export { decodeBase64Url, encodeBase64Url } from './base64url'
+const getTypeName = (value: unknown): string => {
+  const type = typeof value
+
+  if (type !== 'object')
+    return type
+  else if (value === null)
+    return 'null'
+  else
+    return value?.constructor?.name ?? 'object'
+}
+
+export const validateBinaryLike = (source: unknown): Uint8Array => {
+  if (typeof source === 'string')
+    return new TextEncoder().encode(source)
+  else if (source instanceof Uint8Array)
+    return source
+  else if (source instanceof ArrayBuffer)
+    return new Uint8Array(source)
+
+  throw new TypeError(`The input must be a Uint8Array, a string, or an ArrayBuffer. Received a value of the type ${getTypeName(source)}.`)
+}
+
+export const decodeBase64Url = (b64url: string): Uint8Array =>
+  Uint8Array.fromBase64(b64url, { alphabet: 'base64url' })
+
+export const encodeBase64Url = (data: ArrayBuffer | string | Uint8Array): string =>
+  validateBinaryLike(data).toBase64({ alphabet: 'base64url', omitPadding: true })
+
+export const encodeBase64 = (data: ArrayBuffer | string | Uint8Array): string =>
+  validateBinaryLike(data).toBase64()
+
+export const decodeBase64 = (b64: string): Uint8Array =>
+  Uint8Array.fromBase64(b64)
