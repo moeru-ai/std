81064ebef04bd4af7ae121f245b24f950ece4250
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-07-10 01:02:20 +0800

    feat(std/sleep): with abort (#22)

--- packages/std/src/sleep/index.ts
+++ packages/std/src/sleep/index.ts
@@ -1,2 +1,28 @@
 // eslint-disable-next-line @masknet/prefer-timer-id
-export const sleep = async (delay: number): Promise<void> => new Promise(resolve => setTimeout(resolve, delay))
+export const sleep = async (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms))
+
+export const sleepWithAbort = async (ms: number, signal?: AbortSignal): Promise<void> => {
+  if (!signal)
+    return sleep(ms)
+
+  return new Promise<void>((resolve, reject) => {
+    if (signal.aborted) {
+      reject(signal.reason ?? new DOMException('Aborted', 'AbortError'))
+      return
+    }
+
+    const timer = setTimeout(() => {
+      signal.removeEventListener('abort', onAbort)
+      resolve()
+    }, ms)
+
+    signal.addEventListener('abort', onAbort, { once: true })
+
+    // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+    function onAbort() {
+      clearTimeout(timer)
+      signal?.removeEventListener('abort', onAbort)
+      reject(signal?.reason ?? new DOMException('Aborted', 'AbortError'))
+    };
+  })
+}
